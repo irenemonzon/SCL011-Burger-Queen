8d534efa58c24d770578f8342364b3e50d23e314
irenemonzon <irene.monzonm@gmail.com>
2019-12-26 15:57:40 -0300

    menu prueba

--- src/Component/menu.js
+++ src/Component/menu.js
@@ -1,28 +1,49 @@
 import React,{Component}from'react'
 import  db from'../config';
+import{Table,Button}from'reactstrap';
 export default class menú extends Component{
 
     state={
-        item:''
+        items:''
 }
 
     componentDidMount(){
         db.collection("menú").get().then((snapShots)=>{
            this.setState({
-               item:snapShots.docs.map(doc=>{
+               items:snapShots.docs.map(doc=>{
     
                    return {id:doc.id,
                            data:doc.data()}
 
                })
+           },error=>{
+               console.log(error)
            })      
-        })
+        });
     }
 
     render(){
+        const{items}=this.state;
         return(
             <div>
-                <p>Component menu</p>
+                <Table hover className='text-center'>
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Editar</th>
+                            <th>Eliminar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {items && items!==undefined?items.map((item,key)=>(
+                              <tr key={key}>
+                              <td>{item.data.item}</td>
+                              <td><Button color='warning'>Editar</Button></td>
+                              <td><Button color='danger'>Eliminar</Button></td>
+                              </tr>
+                        )):null}
+                    </tbody>
+                </Table>
             </div>
         )
     }
